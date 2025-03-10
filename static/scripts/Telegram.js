8cfa9c9ede88694570d76f92d5a3f12546149191
egor4chan <93943877+egor4chan@users.noreply.github.com>
2025-03-10 21:04:24 +0400

    Add files via upload

--- static/scripts/Telegram.js
+++ static/scripts/Telegram.js
@@ -1,12 +1,13 @@
 var WebApp = window.Telegram.WebApp;
 var username = WebApp.initDataUnsafe.user.username;
 
-var user_id = WebApp.initDataUnsafe.user.id // user id
-var start_param = WebApp.initDataUnsafe.start_param; // refer's id
+//var user_id = WebApp.initDataUnsafe.user.id // user id
+//var start_param = WebApp.initDataUnsafe.start_param; // refer's id
 
 // register user with 'user_id' and 'start_param'
 
-if (username == undefined) 
+if (username == undefined) {
     username = `id${WebApp.initDataUnsafe.user.id}`
+}
 
-document.getElementById('user-username').innerHTML = username;
+document.getElementById('username').innerHTML = username;
